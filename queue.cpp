9cb784306161ee4b92b6936a59b6e8382179e6ae
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-07-11 00:14:48 +0530

    commit

---
+++ queue.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public: 
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Queue {
+    Node* head;
+    Node* tail;
+
+public: 
+    Queue() {
+        head = tail = NULL;
+    }
+
+    void push() {
+        Node* newNode = new Node(data);
+        if(head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop() {
+        if(head == NULL) {
+            cout << "Queue is empty" << endl;
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void front() {
+        return head->data;
+    }
+
+    bool empty() {
+        return head == NULL;
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while(!q.empty()) {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    return 0;
+}
