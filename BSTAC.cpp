13b3c90ab86fdf43580bfa3840316725044a2dcd
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-07-31 21:03:08 +0530

    deleteBST

--- BSTAC.cpp
+++ BSTAC.cpp
@@ -46,12 +46,79 @@ void inorder(Node* root) {
     inorder(root->right);
 }
 
+bool search(Node* root, int key) { //O(height)
+    if(root == NULL) {
+        return false;
+    }
+
+    if(root->data == key) {
+        return true;
+    }
+
+    if(key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+Node* getInorderSuccessor(Node* root) {
+    while(root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* delNode(Node* root, int val) {
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if(val < root->data) { //left subtree
+        root->left = delNode(root->left, val);
+    } else if(val > root->data) { //right subtree
+        root->right = delNode(root->right, val);
+    } else {
+        //root == val
+        //case1: 0 children
+        if(root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+
+        //case2: 1 child
+        if(root->left == NULL || root->right == NULL) {
+            return root->left ? root->left : root->right;
+        }
+
+        //case3: 2 children
+        Node* IS = getInorderSuccessor(root->right);
+        root->data = IS->data;
+        root->right = delNode(root->right, IS->data); //case1, case2
+        return root;
+    }
+}
+
 
 int main() {
-    int arr[6] = {5,1,3,4,2,7};
+    // int arr[6] = {5,1,3,4,2,7};
+    int arr2[9] = {8,5,3,1,4,6,10,11,14};
 
     // Node* root = new Node(arr, 6);
-    Node* root = buildBST(arr, 6);
+    // Node* root = buildBST(arr, 6);
+
+    // inorder(root);
+    // cout << endl;
+
+    // Node* root = buildBST(arr2, 9);
+    // cout << search(root, 6) << endl;
+
+    Node* root = buildBST(arr2, 9);
+    inorder(root);
+    cout << endl;
+
+    delNode(root, 4);
+
     inorder(root);
     cout << endl;
 
