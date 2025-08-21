03c64da0e9d5123de18c1001449d733e28ecc80c
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-08-21 23:07:44 +0530

    commit

--- trieAC.cpp
+++ trieAC.cpp
@@ -47,6 +47,30 @@ public:
     }
 };
 
+bool helper(Trie &trie, string key){
+    if(key.size() == 0) {
+        return true;
+    }
+
+    for(int i=0;i<key.size();i++){
+        string first = key.substr(0, i+1);
+        string second = key.substr(i+1);
+
+        if(trie.search(first) && helper(trie, second)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool wordBreak(vector<string> dict, string key) {
+    Trie trie;
+    for(int i=0;i<dict.size();i++) {
+        trie.insert(dict[i]);
+    }
+    return helper(trie, key);
+}
+
 int main() {
     vector<string> words = {"the", "a", "there", "their", "any", "thee"};
     Trie trie;
